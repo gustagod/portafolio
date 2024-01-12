31b508e4c8707bfd87dd39411827dab89970220e
gustagod <gustavoalain93@outlook.com>
2024-01-11 18:53:35 -0700

    fix

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,10 +3,10 @@ import './style/Education.css'
 
 const Education = () => {
   return (
-    <div>
-<ul>
-  <li><img src=".\itson.png" alt="" /><span>Ingenieria Mecatronica</span><span>2011-2015</span></li>
-  <li><img src=".\academlo.png" alt="" /><span>Desarrollador web Full Stack</span><span>2023-2024</span></li>
+    <div className='education__div'>
+<ul className='education__ul'>
+  <li className='education__li'><img src=".\itson.png" alt="" className='education__img' /><span className='education__span1'>Ingenieria Mecatronica </span><span className='eduaction__span2'>2011-2015</span></li>
+  <li className='education__li'><img src=".\academlo.png" alt="" className='education__img' /><span className='education__span1'>Desarrollador web Full Stack </span><span className='eduaction__span2'>2023-2024</span></li>
 </ul>
     </div>
   )
--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -6,7 +6,7 @@ const Personal = () => {
     <div className='personal__div'>
         <h1 className='personal__h1'>Hola soy <span className='personal__span'>"Gustavo"</span></h1>
         <p className='personal__p'>
-          Soy un programador web fullstack, Gran capacidad de adaptarme a todo tipo de proyectos y mi enfoque principal es programación web, pero con una mente siempre abierta a retos.
+          Soy un programador web fullstack, Gran capacidad de adaptarme a todo tipo de proyectos con una mente siempre abierta a retos.
         </p>
     </div>
 
