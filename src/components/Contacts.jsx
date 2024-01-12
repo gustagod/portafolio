c9d2a59315a7ee49b715e3381840ad4af8becd7f
gustagod <gustavoalain93@outlook.com>
2024-01-11 22:28:32 -0700

    fix3

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './style/Contacts.css'
 
 const Contacts = () => {
+
+  const handellinkeid = () => {
+    window.open('https://www.linkedin.com/in/gustavo-alain-portillo-hernández-b873b35a', '_blank');
+  }
+
+  const handelgithub = () => {
+    window.open('https://github.com/gustagod', '_blank');
+  }
+
+  const handelwhatsapp = () => {
+    window.open('https://api.whatsapp.com/send?phone=5216442110065', '_blank');
+  }
+  const handelcv = () => {
+    window.open('../../cv_esp.pdf/', '_blank');
+  }
+
   return (
-    <div>Contacts</div>
+    <div className='contacts__div'>
+      <ul className='contacts__ul1'>
+        <li className='contacts_ul1li'>
+        <ul className='contacts_ul1ul2'>
+        <li onClick={handellinkeid} className='contacts__ul2_li'><img src="../../Contacts/linkedin.svg" alt="" /><span>Linkedin</span></li>
+        <li onClick={handelgithub} className='contacts__ul2_li'><img src="../../Contacts/github.svg" alt="" /><span>Github</span></li>
+        <li onClick={handelwhatsapp} className='contacts__ul2_li'><img src="../../Contacts/whatsapp.svg" alt="" /><span>Whatsapp</span></li>
+        <li onClick={handelcv} className='contacts__ul2_li'><img src="../../Contacts/cv.svg" alt="" /><span>CV</span></li>
+      </ul>
+        </li>
+        <li className='contacts_ul1li'>
+          <ul className='contacts_ul1ul3'>
+            <li>Gustavo Portillo</li>
+            <li>Telefono: (+52)6442-11-00-65</li>
+            <li>Email: gustavo.alain@gmail.com</li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './style/Projects.css'
 
 const Projects = () => {
+
+  const handelweather=()=>{
+    window.open('https://proyecto2-gamma.vercel.app/','_blank')
+  }
+
+  const handelrickandmorty=()=>{
+    window.open('https://proyecto3-mauve.vercel.app/','_blank')
+  }
+
+  const handelpokemon=()=>{
+    window.open('https://proyecto5-ten.vercel.app/','_blank')
+  }
+
+
   return (
-    <div>Projects</div>
+    <div className='projects__div'>
+      <ul className='projects__ul'>
+        <li className='projects__li' onClick={handelweather}>
+          <img src="../../Projects/weather.PNG" alt="" className='projects__img'/>
+          <span className='projects__span'>Web app para ver el clima de tu localidad, utillizando react,js,html y css</span>
+        </li>
+        <li className='projects__li' onClick={handelrickandmorty}>
+          <img src="../../Projects/rick.PNG" alt="" className='projects__img'/>
+          <span className='projects__span'>Web app de una biblioteca de los personajes de rick y morty, utillizando api, react, js, html y css</span>
+        </li>
+        <li className='projects__li' onClick={handelpokemon}>
+          <img src="../../Projects/poke.PNG" alt="" className='projects__img'/>
+          <span className='projects__span'>Web app de uan biblioteca de los pokemons utilizando api, react, js, html y css</span>
+        </li>
+      </ul>
+    </div>
   )
 }
 
