f6511422a46bff7a1d6d5d593d24287d2c56d8c3
gustagod <gustavoalain93@outlook.com>
2024-01-13 14:14:24 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Personal from './components/Personal'
 import Projects from './components/Projects'
@@ -8,104 +8,83 @@ import Contacts from './components/Contacts'
 
 function App() {
   const [language, setLanguage] = useState('spanish')
-  const [page, setpage] = useState(<Personal lan={language}/>)
+  const [page, setpage] = useState(<Personal lan={language} />)
+  const [id, setid] = useState('Personal')
 
-useEffect(() => {
-  const pag=page.type.name;
-  switch(pag){
-    case 'Personal':
-      setpage(<Personal lan={language} />);
-    break;
-    case 'Projects':
-      setpage(<Projects lan={language} />);
-    break;
-    case 'Languages':
-      setpage(<Languages lan={language} />);
-    break;
-    case 'Education':
-      setpage(<Education lan={language} />);
-    break;
-    case 'Contacts':
-      setpage(<Contacts lan={language} />);
-    break;
+  useEffect(() => {
+    switch (id) {
+      case 'Personal':
+        setpage(<Personal lan={language} />);
+        break;
+
+      case 'Projects':
+        setpage(<Projects lan={language} />)
+        break;
+
+      case 'Lenguages':
+        setpage(<Lenguages lan={language} />)
+        break;
+
+      case 'Education':
+        setpage(<Education lan={language} />)
+        break;
+
+      case 'Contacts':
+        setpage(<Contacts lan={language} />)
+        break;
+    }
+  }, [id,language])
+
+  const change = (p) => {
+    setid(p)
   }
-
-
-}, [language])
-
 
   const handelLanguage = () => {
-    language === 'spanish' ? setLanguage('english'): setLanguage('spanish');
+    language === 'spanish' ? setLanguage('english') : setLanguage('spanish');
 
-  }
-
-  const handelMain=()=>{
-    setpage(<Personal lan={language} />);
-  }
-  const handelProjects=()=>{
-    setpage(<Projects
-      lan={language}
-    />)
-  }
-
-  const handelLenguages=()=>{
-    setpage(<Lenguages
-      lan={language}
-    />)
-  }
-
-  const handelEducation=()=>{
-    setpage(<Education
-      lan={language}
-    />)
-  }
-
-  const handelContacts=()=>{
-    setpage(<Contacts
-      lan={language}
-    />)
   }
 
 
   return (
     <div className='app__div'>
       <button className='app__btnlen' onClick={handelLanguage}>
-      {language === 'spanish' ?  <span className='app__btnlenspan'>Spanish/English</span>  : <span className='app__btnlenspan'>Ingles/Español</span> }
-        </button>
-{ language === 'spanish' ?
-      <header className='app__header'>
-      <ul className='app__ul1'>
-        <li className='app__ul1li'><hr className='app__hr1' /></li>
-        <li className='app__ul1li'>
-          <ul className='app__ul2'>
-            <li><button className='app__btn' onClick={handelMain}><span className='app__ul2lispan'>Inicio</span></button></li>
-            <li><button className='app__btn' onClick={handelProjects}><span className='app__ul2lispan'>Proyectos</span></button></li>
-            <li><button className='app__btn' onClick={handelLenguages}><span className='app__ul2lispan'>Languajes</span></button></li>
-            <li><button className='app__btn' onClick={handelEducation}><span className='app__ul2lispan'>Educación</span></button></li>
-            <li><button className='app__btn' onClick={handelContacts}><span className='app__ul2lispan'>Contactame</span></button></li>
+        {language === 'spanish' ? <span className='app__btnlenspan'>Spanish/English</span> : <span className='app__btnlenspan'>Ingles/Español</span>}
+      </button>
+      {language === 'spanish' ?
+        <header className='app__header'>
+          <ul className='app__ul1'>
+            <li className='app__ul1li'><hr className='app__hr1' /></li>
+            <li className='app__ul1li'>
+              <ul className='app__ul2'>
+                <li><button className='app__btn' onClick={()=> change('Personal')}><span className='app__ul2lispan'>Inicio</span></button></li>
+                <li><button className='app__btn' onClick={()=>change('Projects')}><span className='app__ul2lispan'>Proyectos</span></button></li>
+                <li><button className='app__btn' onClick={()=>change('Lenguages')}><span className='app__ul2lispan'>Languajes</span></button></li>
+                <li><button className='app__btn' onClick={()=>change('Education')}><span className='app__ul2lispan'>Educación</span></button></li>
+                <li><button className='app__btn' onClick={()=>change('Contacts')}><span className='app__ul2lispan'>Contactame</span></button></li>
+              </ul>
+            </li>
           </ul>
-        </li>
-      </ul>
-    </header>
-:
-<header className='app__header'>
-<ul className='app__ul1'>
-  <li className='app__ul1li'><hr className='app__hr1' /></li>
-  <li className='app__ul1li'>
-    <ul className='app__ul2'>
-      <li><button className='app__btn' onClick={handelMain}><span className='app__ul2lispan'>Home</span></button></li>
-      <li><button className='app__btn' onClick={handelProjects}><span className='app__ul2lispan'>Projects</span></button></li>
-      <li><button className='app__btn' onClick={handelLenguages}><span className='app__ul2lispan'>Lenguages</span></button></li>
-      <li><button className='app__btn' onClick={handelEducation}><span className='app__ul2lispan'>Studies</span></button></li>
-      <li><button className='app__btn' onClick={handelContacts}><span className='app__ul2lispan'>Contact</span></button></li>
-    </ul>
-  </li>
-</ul>
-</header>
-}
+        </header>
+        :
+        <header className='app__header'>
+          <ul className='app__ul1'>
+            <li className='app__ul1li'><hr className='app__hr1' /></li>
+            <li className='app__ul1li'>
+              <ul className='app__ul2'>
+                <li><button className='app__btn' onClick={() => change('Personal')}><span className='app__ul2lispan'>Home</span></button></li>
+                <li><button className='app__btn' onClick={() => change('Projects')}><span className='app__ul2lispan'>Projects</span></button></li>
+                <li><button className='app__btn' onClick={() => change('Lenguages')}><span className='app__ul2lispan'>Lenguages</span></button></li>
+                <li><button className='app__btn' onClick={() => change('Education')}><span className='app__ul2lispan'>Studies</span></button></li>
+                <li><button className='app__btn' onClick={() => change('Contacts')}><span className='app__ul2lispan'>Contact</span></button></li>
+
+              </ul>
+            </li>
+          </ul>
+        </header>
+      }
       <div className='app__info'>
         {
-         page
+          page
         }
       </div>
       <hr className='app__hr2' />
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style/Contacts.css'
 
-const Contacts = ({lan}) => {
+const Contacts = ({ lan }) => {
 
   const handellinkeid = () => {
     window.open('https://www.linkedin.com/in/gustavo-alain-portillo-hernández-b873b35a', '_blank');
@@ -15,36 +15,38 @@ const Contacts = ({lan}) => {
     window.open('https://api.whatsapp.com/send?phone=5216442110065', '_blank');
   }
   const handelcv = () => {
-    window.open('../../cv_esp.pdf/', '_blank');
+    lan === 'spanish' ? (
+    window.open('../../cv_esp.pdf/', '_blank')):
+    (window.open('../../cv_eng.pdf/', '_blank'))
   }
 
   return (
     <div className='contacts__div'>
       <ul className='contacts__ul1'>
         <li className='contacts_ul1li'>
-        <ul className='contacts_ul1ul2'>
-        <li onClick={handellinkeid} className='contacts__ul2_li'><img src="../../Contacts/linkedin.svg" alt="" /><span>Linkedin</span></li>
-        <li onClick={handelgithub} className='contacts__ul2_li'><img src="../../Contacts/github.svg" alt="" /><span>Github</span></li>
-        <li onClick={handelwhatsapp} className='contacts__ul2_li'><img src="../../Contacts/whatsapp.svg" alt="" /><span>Whatsapp</span></li>
-        <li onClick={handelcv} className='contacts__ul2_li'><img src="../../Contacts/cv.svg" alt="" /><span>CV</span></li>
-      </ul>
+          <ul className='contacts_ul1ul2'>
+            <li onClick={handellinkeid} className='contacts__ul2_li'><img src="../../Contacts/linkedin.svg" alt="" /><span>Linkedin</span></li>
+            <li onClick={handelgithub} className='contacts__ul2_li'><img src="../../Contacts/github.svg" alt="" /><span>Github</span></li>
+            <li onClick={handelwhatsapp} className='contacts__ul2_li'><img src="../../Contacts/whatsapp.svg" alt="" /><span>Whatsapp</span></li>
+            <li onClick={handelcv} className='contacts__ul2_li'><img src="../../Contacts/cv.svg" alt="" /><span>CV</span></li>
+          </ul>
         </li>
         <li className='contacts_ul1li'>
           {
-            lan === 'spanish' ?(
+            lan === 'spanish' ? (
               <ul className='contacts_ul1ul3'>
-              <li>Gustavo Portillo</li>
-              <li>Telefono: (+52)6442-11-00-65</li>
-              <li>Email: gustavo.alain@gmail.com</li>
-            </ul>
-            ):
-            (
-              <ul className='contacts_ul1ul3'>
-              <li>Gustavo Portillo</li>
-              <li>Phone: (+52)6442-11-00-65</li>
-              <li>Email: gustavo.alain@gmail.com</li>
-            </ul>
-            )
+                <li>Gustavo Portillo</li>
+                <li>Telefono: (+52)6442-11-00-65</li>
+                <li>Email: gustavo.alain@gmail.com</li>
+              </ul>
+            ) :
+              (
+                <ul className='contacts_ul1ul3'>
+                  <li>Gustavo Portillo</li>
+                  <li>Phone: (+52)6442-11-00-65</li>
+                  <li>Email: gustavo.alain@gmail.com</li>
+                </ul>
+              )
           }
 
         </li>
